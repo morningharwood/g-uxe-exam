880238a4b6e2cf60db4713cb97ee76232c83f9fb
Matthew Harwood <matthhar12@gmail.com>
2018-02-16 20:16:35 -0800

    - Added tipe api.

--- src/app/gxe-gallery/containers/master/master.component.ts
+++ src/app/gxe-gallery/containers/master/master.component.ts
@@ -6,7 +6,7 @@ import {
   OnInit,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { GalleryItem, mockGalleryItems } from '../../mock-data';
+import { mockGalleryItems, GalleryItem } from '../../mock-data';
 
 
 
@@ -17,8 +17,9 @@ import { GalleryItem, mockGalleryItems } from '../../mock-data';
   styleUrls: [ './master.component.scss' ],
 })
 export class GalleryMasterComponent implements OnInit {
-  public galleryItems: GalleryItem[] = mockGalleryItems;
+  public galleryItems: GalleryItem[];
   public isActive = false;
+  private itemActive: number;
 
   constructor(private store: Store<any>,
               private builder: AnimationBuilder) {
@@ -26,6 +27,24 @@ export class GalleryMasterComponent implements OnInit {
 
   public ngOnInit(): void {
 
+    const DOCUMENT_ID = '5a878162500cc2001395c413';
+    const YOUR_ORG_SECRET_KEY = 'NWE4NGRhZDM4OTE4OTkwMDEzNjNjOWNi';
+    const YOUR_API_KEY = '3QTLJXHW9ZZWPWGGEUN7OHETO'; // Generated-API-Key-1518661038309
+
+    window.fetch('https://api.tipe.io/api/v1/folder/' + DOCUMENT_ID, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': YOUR_API_KEY,
+        'Tipe-Id': YOUR_ORG_SECRET_KEY
+      }
+    })
+    .then( (res) =>  res.json())
+    .then( (data) => {
+      console.log(data);
+      this.galleryItems = data.documents.map(d => d.blocks);
+      console.log(this.galleryItems);
+    });
   }
   @HostListener('click')
   public addClass() {
@@ -33,5 +52,7 @@ export class GalleryMasterComponent implements OnInit {
   }
 
 
-
+  public activate(item: number) {
+    this.itemActive = item;
+  }
 }
