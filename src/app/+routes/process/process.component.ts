c1636834d23021d410ae83bf587ef4c9384e309b
Matthew Harwood <matthhar12@gmail.com>
2018-02-21 00:51:47 -0800

    - finished process page.

--- src/app/+routes/process/process.component.ts
+++ src/app/+routes/process/process.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./process.component.scss']
 })
 export class GxeProcessComponent implements OnInit {
-
+  public panelOpenState = false;
+  public engiOpen = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public open() {
+    this.panelOpenState = true;
+    window.scrollTo(0 , 0);
+  }
+
+  public close() {
+    this.panelOpenState = false;
+    window.scrollTo(0 , 0);
   }
 
 }
