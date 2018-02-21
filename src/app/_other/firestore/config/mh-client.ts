94422c6f7ace8559cfb9689c8f9f6a145a92ec75
Matthew Harwood <matthhar12@gmail.com>
2018-02-21 04:38:21 -0800

    - updated api keys.

--- src/app/_other/firestore/config/mh-client.ts
+++ src/app/_other/firestore/config/mh-client.ts
@@ -3,12 +3,12 @@ import { FirebaseAppConfig } from 'angularfire2';
 export class MhClientFirestore {
   public static config(): FirebaseAppConfig {
     return {
-      apiKey: 'AIzaSyDMNHtmgsu2dv9MPmUA_EOrt7vS00PW12M',
-      authDomain: 'mh-client.firebaseapp.com',
-      databaseURL: 'https://mh-client.firebaseio.com',
-      projectId: 'mh-client',
-      storageBucket: 'mh-client.appspot.com',
-      messagingSenderId: '985472354315'
+      apiKey: 'AIzaSyD9ZUsu1p-WGtL4KBad27AUGQ0KFHPrh1Y',
+      authDomain: 'gxe-gallery.firebaseapp.com',
+      databaseURL: 'https://gxe-gallery.firebaseio.com',
+      projectId: 'gxe-gallery',
+      storageBucket: 'gxe-gallery.appspot.com',
+      messagingSenderId: '470078309765'
     };
   }
 }
