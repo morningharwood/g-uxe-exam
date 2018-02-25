7a5a49258a0dedc22ce57e43bc02d087ff7728d5
Matthew Harwood <matthhar12@gmail.com>
2018-02-24 21:53:04 -0800

    - learn to use a portal.

--- src/app/uxe-gallery/containers/uxe-gallery-master/uxe-gallery-master.component.ts
+++ src/app/uxe-gallery/containers/uxe-gallery-master/uxe-gallery-master.component.ts
@@ -22,22 +22,27 @@ import {
   Store,
 } from '@ngrx/store';
 import { STANDARD_EASE } from '../../../gxe-gallery/animations/ease.animations';
-import { selectAll, selectFeatureExtended, State } from '../../reducers/uxe-gallery.reducer';
+import {
+  selectAll,
+  selectFeatureExtended,
+  State,
+} from '../../reducers/uxe-gallery.reducer';
 import { UxeGalleryStateService } from '../../services/gallery-service';
 import { Vector2 } from '../../../gxe-gallery/interfaces/vector.interface';
+
 
 export enum AnimationState {
   START = 'start',
   END = 'end',
 }
 
+export const SCALE_SIZE = 2;
+
 @Component({
   selector: 'uxe-gallery-master',
   templateUrl: './uxe-gallery-master.component.html',
-  styleUrls: ['./uxe-gallery-master.component.scss'],
-  animations: [
-
-  ]
+  styleUrls: [ './uxe-gallery-master.component.scss' ],
+  animations: [],
 })
 export class UxeGalleryMasterComponent implements OnInit {
   private obs: Observable<UxeGallery[]>;
@@ -46,19 +51,20 @@ export class UxeGalleryMasterComponent implements OnInit {
   private hostEl: any;
   @ViewChild('container') private containerEl: any;
   private flexWidth: string;
-  private static getCenterY({offsetHeight: parentHeight}, {offsetHeight: childHeight} ): Vector2 {
+
+  private static getCenterY({ offsetHeight: parentHeight }, { offsetHeight: childHeight }): Vector2 {
     return {
       x: 0,
       y: (parentHeight - childHeight) / 2,
     };
   }
 
-  constructor(
-    private store: Store<State>,
-    private galleryStateService: UxeGalleryStateService,
-    private builder: AnimationBuilder,
-    private ngHostEl: ElementRef,
-    private renderer: Renderer2) { }
+  constructor(private store: Store<State>,
+              private galleryStateService: UxeGalleryStateService,
+              private builder: AnimationBuilder,
+              private ngHostEl: ElementRef,
+              private renderer: Renderer2) {
+  }
 
   ngOnInit() {
     this.obs = this.store.pipe(select(selectAll));
@@ -72,31 +78,33 @@ export class UxeGalleryMasterComponent implements OnInit {
     this.hostEl = this.renderer.selectRootElement(this.ngHostEl).nativeElement;
   }
 
-  public selectedItem(item: UxeGallery,
+  public selectedItem(item: number,
                       outerMask: any,
                       innerMask: any,
-                      imgEl: any ) {
-    this.setSelectedItem(item);
+                      imgEl: any) {
+    // this.setSelectedItem(item);
     this.setModalState(true);
 
     const host = this.hostEl.getBoundingClientRect();
     const posA = outerMask.getBoundingClientRect();
     const posB = imgEl.getBoundingClientRect().y;
     const posC = UxeGalleryMasterComponent.getCenterY(this.hostEl, imgEl);
+
     const move = {
       from: posA,
       to: {
-        x: posC.x - posA.x,
-        y: posC.y - posA.y
+        x: item % 2 ? -(posA.width / 2) : (posA.width / 2),
+        y: posC.y - posA.y,
       },
     };
+
     // console.log(posA.x, posA.y, posB, posC);
     // Host will give you the animation end position with centerImageY
     // outerMask will give you starting position.
     // innerMask is the animation element
     // imgEl will give you the height animation values
     // Animate the image from center to topleft
-    this.itemAnimate(move, outerMask);
+    this.itemAnimate(move, innerMask);
     // When animations complete redirect to detail
   }
 
@@ -112,8 +120,9 @@ export class UxeGalleryMasterComponent implements OnInit {
       animate(
         STANDARD_EASE,
         style({
-          transform: `translate3d(${move.to.x}px, ${move.to.y}px, 2px) scale(2)`,
+          transform: `translate3d(${move.to.x}px, ${move.to.y}px, 1px) scale(${SCALE_SIZE})`,
           zIndex: 1,
+          height: '134px'
         }),
       ),
     ]).create(el);
