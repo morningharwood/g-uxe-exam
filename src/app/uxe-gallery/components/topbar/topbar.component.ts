f7dc4e3e09d15c580d16419a5f409a8c42d4290a
Matthew Harwood <matthhar12@gmail.com>
2018-02-25 18:03:16 -0800

    - added topbar animations.

--- src/app/uxe-gallery/components/topbar/topbar.component.ts
+++ src/app/uxe-gallery/components/topbar/topbar.component.ts
@@ -1,9 +1,37 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { STANDARD_EASE } from '../../../gxe-gallery/animations/ease.animations';
 
 @Component({
   selector: 'uxe-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
+  animations: [
+    trigger('enterBar', [
+      state('0', style({
+        top: '-80px',
+      })),
+      state('1', style({
+        top: '0',
+      })),
+      transition('0 => 1', [
+        animate(STANDARD_EASE, style({
+          top: '0',
+        })),
+      ]),
+      transition('1 => 0', [
+        animate(STANDARD_EASE, style({
+          top: '-80',
+        })),
+      ]),
+    ]),
+  ],
 })
 export class TopbarComponent implements OnInit {
 
